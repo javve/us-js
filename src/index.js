38a5d4483bbc330d7133203ebf1bf784aacc0d8a
Jonny Strömberg <jonny.stromberg@gmail.com>
2016-11-14 20:44:55 +0100

    Support more styles

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ const assign = require('object-assign'),
 
 class Switcher {
   constructor(id, options) {
+    this.style = 'default';
     assign(this, options);
     this.states = states(this);
 
     this.el = document.getElementById(id);
     this.el.addEventListener('click', (e) => { this.click(e.target); });
+    this.el.classList.add(this.style);
     this.static()
     this.setInitialState()
   }
@@ -45,23 +47,27 @@ class Switcher {
       , nextSize = size.get(next)
       , me = this;
 
+    if (current == next) {
+      return;
+    }
+
     size.set(this.el, currentSize);
     this.states.next(next);
     setTimeout(() => {
       this.animate()
-        size.set(this.el, nextSize);
-        this.states.previous(current);
-        if (current) {
-          this.states.hide(current);
-        }
-        this.states.show(next);
-        next.addEventListener(transition.end, function fn(e) {
-          me.el.removeEventListener(transition.end, fn);
-          me.static();
-          me.states.clearNext(next);
-          me.states.clearPrevious(current);
-        });
-    }, 100);
+      size.set(this.el, nextSize);
+      this.states.previous(current);
+      if (current) {
+        this.states.hide(current);
+      }
+      this.states.show(next);
+      next.addEventListener(transition.end, function fn(e) {
+        me.el.removeEventListener(transition.end, fn);
+        me.static();
+        me.states.clearNext(next);
+        me.states.clearPrevious(current);
+      });
+    }, 10);
   }
   animate() {
     this.el.classList.remove('static');
