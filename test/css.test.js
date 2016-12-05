2f71ebbf7d259fcfa664ca3d51d2d12e7f11f2c0
Jonny Strömberg <jonny.stromberg@gmail.com>
2016-12-05 23:15:52 +0100

    Remove arrow functions

--- test/css.test.js
+++ test/css.test.js
@@ -1,41 +1,41 @@
 const expect = require('chai').expect
     , css = require('../src/utils/css.js');
 
-describe('CSS', () => {
-  beforeEach(() => {
+describe('CSS', function() {
+  beforeEach(function() {
     this.el = document.createElement('div');
     document.body.appendChild(this.el);
   });
-  afterEach(() => {
+  afterEach(function() {
     this.el.remove()
   });
 
-  it('should set opacity', () => {
+  it('should set opacity', function() {
     let res = css.set(this.el, { opacity: { val: '0.3', unit: '' }});
     expect(this.el.style.opacity).to.equal('0.3');
   });
 
-  it('should set translateY', () => {
+  it('should set translateY', function() {
     css.set(this.el, { translateY: {val: 10, unit: 'px'}});
     expect(this.el.style.transform).to.equal('translateY(10px)');
   });
 
-  it('should set translateY and rotate', () => {
+  it('should set translateY and rotate', function() {
     css.set(this.el, {translateY: {val: 10, unit:'px'}, rotate: {val:20, unit: 'deg'}});
     expect(this.el.style.transform).to.equal('translateY(10px) rotate(20deg)');
   });
 
-  it('should set translateY and scale', () => {
+  it('should set translateY and scale', function() {
     css.set(this.el, {translateY: { val: 10, unit: 'px'}, scale: {val:1.3, unit:''}});
     expect(this.el.style.transform).to.equal('translateY(10px) scale(1.3)');
   });
 
-  it('should set position', () => {
+  it('should set position', function() {
     let res = css.set(this.el, { position: { val: 'absolute', unit: '' }});
     expect(this.el.style.position).to.equal('absolute');
   });
 
-  it('should set full style', () => {
+  it('should set full style', function() {
     css.set(this.el, {
       translateY: { val:10, unit:'px'},
       scale: { val:1.3, unit: ''},
@@ -45,7 +45,7 @@ describe('CSS', () => {
     expect(this.el.style.transform).to.equal('translateY(10px) scale(1.3)');
   });
 
-  it('should parse transform style with multiple values', () => {
+  it('should parse transform style with multiple values', function() {
     this.el.style.transform = "translateY(10px) scale(1.3) rotateX(-10deg)";
     expect(css.parseTransform(this.el)).to.deep.equal({
       translateY: { val: 10, unit: 'px' },
@@ -54,7 +54,7 @@ describe('CSS', () => {
     });
   });
 
-  it('should generate transform string', () => {
+  it('should generate transform string', function() {
     let transformString = css.generateTransformString({
       translateY: { val: '10', unit: 'px' },
       opacity: { val: 2, unit: '' },
@@ -64,53 +64,53 @@ describe('CSS', () => {
     expect(transformString).to.equal("translateY(10px) scale(1.3) rotateX(-10deg)");
   });
 
-  it('should parse transform style', () => {
+  it('should parse transform style', function() {
     this.el.style.transform = "translateY(10px)";
     expect(css.parseTransform(this.el)).to.deep.equal({
       translateY: { val: 10, unit: 'px' }
     });
   });
 
-  it('should parse transform style when is not set', () => {
+  it('should parse transform style when is not set', function() {
     expect(css.parseTransform(this.el)).to.equal(null);
   });
 
-  it('should parse px value', () => {
+  it('should parse px value', function() {
     expect(css.parseVal('10px')).to.deep.equal({
       val: 10,
       unit: 'px'
     });
   });
 
-  it('should parse negative px value', () => {
+  it('should parse negative px value', function() {
     expect(css.parseVal('-20px')).to.deep.equal({
       val: -20,
       unit: 'px'
     });
   });
 
-  it('should parse degrees value', () => {
+  it('should parse degrees value', function() {
     expect(css.parseVal('150deg')).to.deep.equal({
       val: 150,
       unit: 'deg'
     });
   });
 
-  it('should parse negative degrees value', () => {
+  it('should parse negative degrees value', function() {
     expect(css.parseVal('-54deg')).to.deep.equal({
       val: -54,
       unit: 'deg'
     });
   });
 
-  it('should opacity value', () => {
+  it('should opacity value', function() {
     expect(css.parseVal('1')).to.deep.equal({
       val: 1,
       unit: ''
     });
   });
 
-  it('should opacity value with decimals', () => {
+  it('should opacity value with decimals', function() {
     expect(css.parseVal('0.3')).to.deep.equal({
       val: 0.3,
       unit: ''
--- test/size.test.js
+++ test/size.test.js
@@ -2,36 +2,38 @@ const expect = require('chai').expect
     , style = require('./style.scss')
     , size = require('../src/utils/size.js');
 
-describe('Size', () => {
-  beforeEach(() => {
+describe('Size', function() {
+  console.log(this);
+  beforeEach(function() {
+    console.log(this);
     this.el = document.createElement('div');
     document.body.appendChild(this.el);
   });
-  afterEach(() => {
+  afterEach(function() {
     this.el.remove()
   });
 
-  it('should height via css', () => {
+  it('should height via css', function() {
     this.el.classList.add('test-height');
     expect(size.height(this.el)).to.equal(800);
   });
 
-  it('should height via css with padding', () => {
+  it('should height via css with padding', function() {
     this.el.classList.add('test-height-padding');
     expect(size.height(this.el)).to.equal(820);
   });
 
-  it('should height via css with padding and border-box', () => {
+  it('should height via css with padding and border-box', function() {
     this.el.classList.add('test-height-box-sizing');
     expect(size.height(this.el)).to.equal(820);
   });
 
-  it('should height via css with scale', () => {
+  it('should height via css with scale', function() {
     this.el.classList.add('test-height-scale');
     expect(size.height(this.el)).to.equal(820);
   });
 
-  it('should height with content', () => {
+  it('should height with content', function() {
     content = document.createElement('div');
     content.innerHTML = 'hej';
     this.el.appendChild(content);
